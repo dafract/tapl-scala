eab18c28bdb0b98e4d062e09699b398e8bfeec88
dafract <dafract@gmail.com>
2021-10-02 02:04:59 +0900

    feat: create single step evaluator

--- src/main/scala/ch04/Arith.scala
+++ src/main/scala/ch04/Arith.scala
@@ -1,16 +1,45 @@
 package ch04
 
-class Arith {
+import ch04.Term._
+import ch04.NoRuleApplies
+
+class Arith() {
   def isNumericValue(t: Term): Boolean = t match {
-    case Term.Zero => true
-    case Term.Succ(t) => isNumericValue(t)
+    case Zero => true
+    case Succ(t) => isNumericValue(t)
     case _ => false
   }
 
   def isValue(t: Term): Boolean = t match {
-    case Term.True => true
-    case Term.False => true
+    case True => true
+    case False => true
     case t if isNumericValue(t) => true
     case _ => false
+  }
+
+  def eval1(t: Term): Term = t match {
+    case If(True, t2: Term, t3: Term) => t2
+    case If(False, t2: Term, t3: Term) => t3
+    case If(t: Term, t2: Term, t3: Term) => {
+      val t1 = eval1(t)
+      If(t1, t2, t3)
+    }
+    case Succ(t) => {
+      val t1 = eval1(t)
+      Succ(t1)
+    }
+    case Pred(Zero) => Zero
+    case Pred(Succ(nv)) if isNumericValue(nv) => nv
+    case Pred(t) => {
+      val t1 = eval1(t)
+      Pred(t1)
+    }
+    case IsZero(Zero) => True
+    case IsZero(Succ(nv)) if isNumericValue(nv) => False
+    case IsZero(t) => {
+      val t1 = eval1(t)
+      IsZero(t1)
+    }
+    case _ => throw NoRuleApplies()
   }
 }
---
+++ src/main/scala/ch04/NoRuleApplies.scala
@@ -0,0 +1,3 @@
+package ch04
+
+class NoRuleApplies extends Error
